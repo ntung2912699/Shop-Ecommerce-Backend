ca2d609505537c6aac63c963e5101d5cee2225b5
TungNV21 <104347621+TungNV21@users.noreply.github.com>
2022-09-05 14:44:15 +0700

    upadate config

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+\Illuminate\Support\Facades\Auth::routes();
 Route::get('/home', [\App\Http\Controllers\Client\Index\HomeController::class, 'index'])->name('home');
